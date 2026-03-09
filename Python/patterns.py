4d96951a308197ad4f9b0e4cf50addf92a983666
Sriraaj27 <sriraaj27@gmail.com>
2026-03-09 19:42:09 +0530

    Patterns Upadted 9-3-26

--- Python/patterns.py
+++ Python/patterns.py
@@ -1,15 +1,17 @@
-# for i in range(3):                # 1 2 3
-#     for j in range(1,4):          # 2 3 4
-#         print(i+j,end=' ')        # 3 4 5
-#     print()
-
-for i in range(3):
-    for j in range(1,4):
-        print(chr(j+96),end=' ')
+for a in range(3):                # 1 2 3
+    for b in range(1,4):          # 2 3 4
+        print(a+b,end=' ')        # 3 4 5
     print()
-    
 
-for x in range(1,4):
-    for y in range(1,4):
-        print(chr(y+64),end=' ')
+print("---------------")
+for i in range(3):                      # a b c
+    for j in range(1,4):                # b c d
+        print(chr(i+j+96),end=' ')      # c d e
+    print()
+
+print("--------------")
+
+for x in range(3):                      # A B C
+    for y in range(1,4):                # B C D
+        print(chr(x+y+64),end=' ')      # C D E
     print()    
